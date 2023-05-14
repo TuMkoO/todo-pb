3fd327d5c2af62df422d1c3eef82ee90aade300d
TuMkoO <mr.t09@mail.ru>
2023-05-14 17:40:38 +0300

    add todoitem component

--- src/components/AppTodoItem.vue
+++ src/components/AppTodoItem.vue
@@ -1,3 +1,13 @@
-<template></template>
+<template>
+  <li class="todo-item todo-item--done">
+    <div class="todo-item__status">
+      <i class="bi bi-check2"></i>
+    </div>
+    <span class="todo-item__text">Learn the basics of Vue</span>
+    <button class="todo-item__remove-button">
+      <i class="bi bi-trash3"></i>
+    </button>
+  </li>
+</template>
 <script lang="ts"></script>
 <style></style>
--- src/components/AppTodoList.vue
+++ src/components/AppTodoList.vue
@@ -1,33 +1,14 @@
 <template>
   <ul class="todo-list">
-    <li class="todo-item todo-item--done">
-      <div class="todo-item__status">
-        <i class="bi bi-check2"></i>
-      </div>
-      <span class="todo-item__text">Learn the basics of Vue</span>
-      <button class="todo-item__remove-button">
-        <i class="bi bi-trash3"></i>
-      </button>
-    </li>
-    <li class="todo-item">
-      <div class="todo-item__status">
-        <i class="bi bi-check2"></i>
-      </div>
-      <span class="todo-item__text">Learn the basics of Typescript</span>
-      <button class="todo-item__remove-button">
-        <i class="bi bi-trash3"></i>
-      </button>
-    </li>
-    <li class="todo-item">
-      <div class="todo-item__status">
-        <i class="bi bi-check2"></i>
-      </div>
-      <span class="todo-item__text">Subscribe to the channel</span>
-      <button class="todo-item__remove-button">
-        <i class="bi bi-trash3"></i>
-      </button>
-    </li>
+    <AppTodoItem />
   </ul>
 </template>
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppTodoItem from "./AppTodoItem.vue";
+
+export default defineComponent({
+  components: { AppTodoItem },
+});
+</script>
 <style></style>
